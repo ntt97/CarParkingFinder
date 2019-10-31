e78e9736d44064bb5572b562680f36c903fd26ef
ntt97 <nthanhto1006@gmail.com>
2019-10-31 22:44:06 +0700

    (3)finish

--- screens/Map.js
+++ screens/Map.js
@@ -1,267 +1,458 @@
-import React, { Component } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  Dimensions,
-  TouchableWithoutFeedback,
-  Picker,
-  FlatList
-} from "react-native";
-import MapView from "react-native-maps";
-import { Ionicons } from "@expo/vector-icons";
-const {Marker} = MapView;
-const { height, width } = Dimensions.get("screen");
-const parkings = [
+import React, { Component } from 'react'
+import { Text, StyleSheet, View, FlatList, Dimensions, TouchableOpacity, TouchableWithoutFeedback } from 'react-native'
+import MapView from 'react-native-maps';
+import Modal from 'react-native-modal';
+import Dropdown from 'react-native-modal-dropdown';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+
+import * as theme from '../theme';
+
+const { Marker } = MapView;
+const { height, width } = Dimensions.get('screen');
+const parkingsSpots = [
   {
     id: 1,
-    title: "Parking 1",
+    title: 'Parking 1',
     price: 5,
     rating: 4.2,
     spots: 20,
     free: 10,
     coordinate: {
-      latitude: 37.78845,
-      longitude: -122.4344
-    }
+      latitude: 37.78735,
+      longitude: -122.4334,
+    },
+    description: `Description about this parking lot
+Open year 2018
+Secure with CTV`,
   },
   {
     id: 2,
-    title: "Parking 2",
+    title: 'Parking 2',
     price: 7,
     rating: 3.8,
     spots: 25,
     free: 20,
     coordinate: {
-      latitude: 37.78615,
-      longitude: -122.4314
-    }
+      latitude: 37.78845,
+      longitude: -122.4344,
+    },
+    description: `Description about this parking lot
+Open year 2014
+Secure with CTV`,
   },
   {
     id: 3,
-    title: "Parking 3",
+    title: 'Parking 3',
     price: 10,
-    rating: 4.0,
+    rating: 4.9,
     spots: 50,
     free: 25,
     coordinate: {
-      latitude: 37.78815,
-      longitude: -122.4314
-    }
-  }
+      latitude: 37.78615,
+      longitude: -122.4314,
+    },
+    description: `Description about this parking lot
+Open year 2019
+Secure with CTV`,
+  },
 ];
-export default class Map extends Component {
+
+class ParkingMap extends Component {
   state = {
     hours: {},
-    active:null
-  };
-  componentDidMount() {
+    active: null,
+    activeModal: null,
+  }
+
+  componentWillMount() {
+    const { parkings } = this.props;
     const hours = {};
-    parkings.map(parking => {
-      hours[parking.id] = 1;
-    });
+
+    parkings.map(parking => {hours[parking.id] = 1});
+
     this.setState({ hours });
   }
+  
+  handleHours = (id, value) => {
+    const { hours } = this.state;
+    hours[id] = value;
+
+    this.setState({ hours });
+  }
+
   renderHeader() {
     return (
       <View style={styles.header}>
-        <Text>Header</Text>
-      </View>
-    );
-  }
-  renderParking(item) {
-    const { hours } = this.state;
-    return (
-      <TouchableWithoutFeedback onPress={()=>this.setState({active:item.id})}>
-        <View key={`parking-${item.id}`} style={styles.parking}>
-        <View style={{ flex: 1, flexDirection: "column" }}>
-          <Text>
-            x {item.spots} {item.title}
-          </Text>
-          {/* <Picker
-              selectedValue={this.state.hours[item.id ||1]}
-              style={{height: 50, width: 120}}
-              itemStyle={{borderWidth:1,borderColor:'grey'}}
-              onValueChange={(itemValue, itemIndex) =>
-                this.setState({hours: {...this.state.hours,[item.id]:itemValue}})
-              }>
-              <Picker.Item label="01:00" value={1} />
-              <Picker.Item label="02:00" value={2} />
-              <Picker.Item label="03:00" value={3} />
-              <Picker.Item label="04:00" value={4} />
-              <Picker.Item label="05:00" value={5} />
-              <Picker.Item label="06:00" value={6} />
-          </Picker> */}
-          <View
-            style={{
-              width: 100,
-              borderRadius: 6,
-              borderColor: "grey",
-              borderWidth: 0.5,
-              padding: 4
-            }}
-          >
-            <Text style={{ fontSize: 16 }}>05:00 hrs</Text>
-          </View>
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Text style={styles.headerTitle}>Detected location</Text>
+          <Text style={styles.headerLocation}>San Francisco, US</Text>
         </View>
-        <View style={{ flex: 1, flexDirection: "row" }}>
-          <View style={{ flex: 1, justifyContent: "space-around" }}>
-            <View
-              style={{ flex: 1, flexDirection: "row", alignItems: "center" }}
-            >
-              <Ionicons color="#7D81BA" size={18} name="ios-pricetag" />
-              <Text style={{ marginLeft: 12 }}>${item.price}</Text>
-            </View>
-
-            <View
-              style={{ flex: 1, flexDirection: "row", alignItems: "center" }}
-            >
-              <Ionicons color="#7D81BA" size={18} name="ios-star" />
-              <Text style={{ marginLeft: 12 }}>{item.rating}</Text>
-            </View>
-          </View>
-
-          <TouchableWithoutFeedback onPress={() => {}}>
-            <View style={styles.buy}>
-              <View>
-                <Text
-                  style={{
-                    flex: 2,
-                    fontSize: 25,
-                    color: "#fff",
-                    fontWeight: "500"
-                  }}
-                >
-                  ${item.price * 2}
-                </Text>
-                <Text style={{ color: "#fff" }}>
-                  {item.price}x{hours[item.id]} hrs
-                </Text>
-              </View>
-              <View
-                style={{
-                  flex: 1,
-                  justifyContent: "center",
-                  alignItems: "center"
-                }}
-              >
-                <Text
-                  style={{ fontSize: 25, color: "#fff", fontWeight: "500" }}
-                >
-                  >
-                </Text>
-              </View>
-            </View>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-end', }}>
+          <TouchableWithoutFeedback>
+            <Ionicons name="ios-menu" size={theme.SIZES.icon * 1.5} />
           </TouchableWithoutFeedback>
         </View>
       </View>
-      </TouchableWithoutFeedback>
-      
-    );
+    )
   }
-  renderParkings() {
+
+  renderParking = (item) => {
+    const { hours } = this.state;
+    const totalPrice = item.price * hours[item.id];
+
+    return (
+      <TouchableWithoutFeedback key={`parking-${item.id}`} onPress={() => this.setState({ active: item.id })} >
+        <View style={[styles.parking, styles.shadow]}>
+          <View style={styles.hours}>
+            <Text style={styles.hoursTitle}>x {item.spots} {item.title}</Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              {this.renderHours(item.id)}
+              <Text style={{ color: theme.COLORS.gray }}>hrs</Text>
+            </View>
+          </View>
+          <View style={styles.parkingInfoContainer}>
+            <View style={styles.parkingInfo}>
+              <View style={styles.parkingIcon}>
+                <Ionicons name='ios-pricetag' size={theme.SIZES.icon} color={theme.COLORS.gray} />
+                <Text style={{ marginLeft: theme.SIZES.base }}> ${item.price}</Text>
+              </View>
+              <View style={styles.parkingIcon}>
+                <Ionicons name='ios-star' size={theme.SIZES.icon} color={theme.COLORS.gray} />
+                <Text style={{ marginLeft: theme.SIZES.base }}> {item.rating}</Text>
+              </View>
+            </View>
+            <TouchableOpacity style={styles.buy} onPress={() => this.setState({ activeModal: item })}>
+              <View style={styles.buyTotal}>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <FontAwesome name='dollar' size={theme.SIZES.icon * 1.25} color={theme.COLORS.white} />
+                  <Text style={styles.buyTotalPrice}>{totalPrice}</Text>
+                </View>
+                <Text style={{ color: theme.COLORS.white }}>
+                  {item.price}x{hours[item.id]} hrs
+                </Text>
+              </View>
+              <View style={styles.buyBtn}>
+                <FontAwesome name='angle-right' size={theme.SIZES.icon * 1.75} color={theme.COLORS.white} />
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    )
+  }
+
+  renderParkings = () => {
     return (
       <FlatList
-        horizontal={true}
-        style={styles.parkings}
+        horizontal
         pagingEnabled
         scrollEnabled
         showsHorizontalScrollIndicator={false}
-        data={parkings}
+        scrollEventThrottle={16}
+        snapToAlignment="center"
+        style={styles.parkings}
+        data={this.props.parkings}
+        extraData={this.state}
+        keyExtractor={(item, index) => `${item.id}`}
         renderItem={({ item }) => this.renderParking(item)}
-        keyExtractor={item => `parking-${item.id}`}
       />
+    )
+  }
+
+  renderHours(id) {
+    const { hours } = this.state;
+    const availableHours = [1, 2, 3, 4, 5, 6];
+
+    return (
+      <Dropdown
+        defaultIndex={0}
+        options={availableHours}
+        style={styles.hoursDropdown}
+        defaultValue={`0${hours[id]}:00` || '01:00'}
+        dropdownStyle={styles.hoursDropdownStyle}
+        onSelect={(index, value) => this.handleHours(id, value)}
+        renderRow={(option) => (
+          <Text style={styles.hoursDropdownOption}>{`0${option}:00`}</Text>
+        )}
+        renderButtonText={option => `0${option}:00`}
+      />
+    )
+  }
+
+  renderModal() {
+    const { activeModal, hours } = this.state;
+
+    if (!activeModal) return null;
+
+    return (
+      <Modal
+        isVisible
+        useNativeDriver
+        style={styles.modalContainer}
+        backdropColor={theme.COLORS.overlay}
+        onBackButtonPress={() => this.setState({ activeModal: null })}
+        onBackdropPress={() => this.setState({ activeModal: null })}
+        onSwipeComplete={() => this.setState({ activeModal: null })}
+      >
+        <View style={styles.modal}>
+          <View>
+            <Text style={{ fontSize: theme.SIZES.font * 1.5 }}>
+              {activeModal.title}
+            </Text>
+          </View>
+          <View style={{ paddingVertical: theme.SIZES.base }}>
+            <Text style={{ color: theme.COLORS.gray, fontSize: theme.SIZES.font * 1.1 }}>
+              {activeModal.description}
+            </Text>
+          </View>
+          <View style={styles.modalInfo}>
+            <View style={[styles.parkingIcon, {justifyContent: 'flex-start'} ]}>
+              <Ionicons name='ios-pricetag' size={theme.SIZES.icon * 1.1} color={theme.COLORS.gray} />
+              <Text style={{ fontSize: theme.SIZES.icon * 1.15 }}> ${activeModal.price}</Text>
+            </View>
+            <View style={[styles.parkingIcon, {justifyContent: 'flex-start'} ]}>
+              <Ionicons name='ios-star' size={theme.SIZES.icon * 1.1} color={theme.COLORS.gray} />
+              <Text style={{ fontSize: theme.SIZES.icon * 1.15 }}> {activeModal.rating}</Text>
+            </View>
+            <View style={[styles.parkingIcon, {justifyContent: 'flex-start'} ]}>
+              <Ionicons name='ios-pin' size={theme.SIZES.icon * 1.1} color={theme.COLORS.gray} />
+              <Text style={{ fontSize: theme.SIZES.icon * 1.15 }}> {activeModal.price}km</Text>
+            </View>
+            <View style={[styles.parkingIcon, {justifyContent: 'flex-start'} ]}>
+              <Ionicons name='ios-car' size={theme.SIZES.icon * 1.3} color={theme.COLORS.gray} />
+              <Text style={{ fontSize: theme.SIZES.icon * 1.15 }}> {activeModal.free}/{activeModal.spots}</Text>
+            </View>
+          </View>
+          <View style={styles.modalHours}>
+            <Text style={{ textAlign: 'center', fontWeight: '500' }}>Choose your Booking Period:</Text>
+            <View style={styles.modalHoursDropdown}>
+              {this.renderHours(activeModal.id)}
+              <Text style={{ color: theme.COLORS.gray }}>hrs</Text>
+            </View>
+          </View>
+          <View>
+            <TouchableOpacity style={styles.payBtn}>
+              <Text style={styles.payText}>
+                Proceed to pay ${activeModal.price * hours[activeModal.id]}
+              </Text>
+              <FontAwesome name='angle-right' size={theme.SIZES.icon * 1.75} color={theme.COLORS.white} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     );
   }
 
   render() {
+    const { currentPosition, parkings } = this.props;
+
     return (
       <View style={styles.container}>
         {this.renderHeader()}
         <MapView
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0122,
-            longitudeDelta: 0.0121
-          }}
+          initialRegion={currentPosition}
           style={styles.map}
         >
           {parkings.map(parking => (
-              <Marker
-                key={`marker-${parking.id}`}
-                coordinate={parking.coordinate}
-              >
-                <View style={[styles.marker,this.state.active===parking.id? styles.active:null]}>
-                    <Text style={{color:"#B40815"}}>${parking.price}</Text>
-                    <Text>({parking.free}/{parking.spots})</Text>
+            <Marker
+              key={`marker-${parking.id}`}
+              coordinate={parking.coordinate}
+            >
+              <TouchableWithoutFeedback onPress={() => this.setState({ active: parking.id })} >
+                <View style={[
+                  styles.marker,
+                  styles.shadow,
+                  this.state.active === parking.id ? styles.active : null
+                ]}>
+                  <Text style={styles.markerPrice}>${parking.price}</Text>
+                  <Text style={styles.markerStatus}> ({parking.free}/{parking.spots})</Text>
                 </View>
-              </Marker>
-           ))}
+              </TouchableWithoutFeedback>
+            </Marker>
+          ))}
         </MapView>
-        
         {this.renderParkings()}
+        {this.renderModal()}
       </View>
-    );
+    )
   }
 }
+
+ParkingMap.defaultProps = {
+  currentPosition: {
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0122,
+    longitudeDelta: 0.0121,
+  },
+  parkings: parkingsSpots,
+}
+
+export default ParkingMap;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: "center",
-    // alignItems: "center",
-    backgroundColor: "#fff"
+    backgroundColor: theme.COLORS.white,
   },
   header: {
-    flex: 0.5
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingHorizontal: theme.SIZES.base * 2,
+    paddingTop: theme.SIZES.base * 2.5,
+    paddingBottom: theme.SIZES.base * 1.5,
+  },
+  headerTitle: {
+    color: theme.COLORS.gray,
+  },
+  headerLocation: {
+    fontSize: theme.SIZES.font,
+    fontWeight: '500',
+    paddingVertical: theme.SIZES.base / 3,
   },
   map: {
-    flex: 3
+    flex: 3,
   },
   parkings: {
-    position: "absolute",
+    position: 'absolute',
     right: 0,
     left: 0,
-    bottom: 24
+    bottom: 0,
+    paddingBottom: theme.SIZES.base * 2,
   },
   parking: {
-    backgroundColor: "#fff",
+    flexDirection: 'row',
+    backgroundColor: theme.COLORS.white,
     borderRadius: 6,
-    padding: 12,
-    marginHorizontal: 24,
-    width: width - 24 * 2,
-    flexDirection: "row",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 12,
-    },
-    shadowOpacity: 0.5,
-    shadowRadius: 4,
-    elevation: 16,
+    padding: theme.SIZES.base,
+    marginHorizontal: theme.SIZES.base * 2,
+    width: width - (24 * 2),
   },
   buy: {
     flex: 1,
-    backgroundColor: "#840B15",
+    flexDirection: 'row',
+    paddingHorizontal: theme.SIZES.base * 1.5,
+    paddingVertical: theme.SIZES.base,
+    backgroundColor: theme.COLORS.red,
     borderRadius: 6,
-    padding: 12,
-    flexDirection: "row"
   },
-  marker:{
+  buyTotal: {
+    flex: 2,
+    justifyContent: 'space-evenly',
+  },
+  buyTotalPrice: {
+    color: theme.COLORS.white,
+    fontSize: theme.SIZES.base * 2,
+    fontWeight: '600',
+    paddingLeft: theme.SIZES.base / 4,
+  },
+  buyBtn: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  marker: {
+    flexDirection: 'row',
+    backgroundColor: theme.COLORS.white,
+    borderRadius: theme.SIZES.base * 2,
+    paddingVertical: 12,
+    paddingHorizontal: theme.SIZES.base * 2,
+    borderWidth: 1,
+    borderColor: theme.COLORS.white,
+  },
+  markerPrice: { color: theme.COLORS.red, fontWeight: 'bold', },
+  markerStatus: { color: theme.COLORS.gray },
+  shadow: {
+    shadowColor: theme.COLORS.black,
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+  },
+  active: {
+    borderColor: theme.COLORS.red,
+  },
+  hours: {
+    flex: 1,
+    flexDirection: 'column',
+    marginLeft: theme.SIZES.base / 2,
+    justifyContent: 'space-evenly',
+  },
+  hoursTitle: {
+    fontSize: theme.SIZES.text,
+    fontWeight: '500',
+  },
+  hoursDropdown: {
+    borderRadius: theme.SIZES.base / 2,
+    borderColor: theme.COLORS.overlay,
+    borderWidth: 1,
+    padding: theme.SIZES.base,
+    marginRight: theme.SIZES.base / 2,
+  },
+  hoursDropdownOption: {
+    padding: 5,
+    fontSize: theme.SIZES.font * 0.8,
+  },
+  hoursDropdownStyle: {
+    marginLeft: -theme.SIZES.base,
+    paddingHorizontal: theme.SIZES.base / 2,
+    marginVertical: -(theme.SIZES.base + 1),
+  },
+  parkingInfoContainer: { flex: 1.5, flexDirection: 'row' },
+  parkingInfo: {
+    justifyContent: 'space-evenly',
+    marginHorizontal: theme.SIZES.base * 1.5,
+  },
+  parkingIcon: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  modalContainer: {
+    margin: 0,
+    justifyContent: 'flex-end',
    
-    flex:1,   
-    flexDirection:"row",
-    borderRadius:24,
-    paddingVertical:12,
-    paddingHorizontal:24,
-    backgroundColor:"#fff",
-    borderWidth:1,
-    borderColor:'grey'
-    
-
   },
-  active:{
-    borderColor:'#B40815',
-    borderWidth:1
+  modal: {
+    flexDirection: 'column',
+    height: height * 0.75,
+    padding: theme.SIZES.base * 2,
+    backgroundColor: theme.COLORS.white,
+    borderTopLeftRadius: theme.SIZES.base,
+    borderTopRightRadius: theme.SIZES.base,
+    
+  },
+  modalInfo: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    paddingVertical: theme.SIZES.base,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderTopColor: theme.COLORS.overlay,
+    borderBottomColor: theme.COLORS.overlay,
+  },
+  modalHours: {
+    paddingVertical: height * 0.11,
+  },
+  modalHoursDropdown: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: theme.SIZES.base,
+  },
+  payBtn: {
+    borderRadius: 6,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: theme.SIZES.base * 1.5,
+    backgroundColor: theme.COLORS.red,
+    marginBottom:10
+  },
+  payText: {
+    fontWeight: '600',
+    fontSize: theme.SIZES.base * 1.5,
+    color: theme.COLORS.white,
   }
-});
+})
